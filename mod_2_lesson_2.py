0bb13214cfa9724aada1c670b7e0d48f80848666
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-10-26 21:11:44 +0300

    Middle stop
    Middle stop

--- mod_2_lesson_2.py
+++ mod_2_lesson_2.py
@@ -1,11 +1,57 @@
-x = int(input("Введіть перше число: "))
-y = int(input("Введіть друге число: "))
-try:
-    result = x/y
-    print("x/y = ", result)
-except ZeroDivisionError:
-    print("Ділення на 0 забороненно!")
-else:
-    print("It is ok")
-finally:
-    print("Finally block")
+# x = int(input("Введіть перше число: "))
+# y = int(input("Введіть друге число: "))
+# try:
+#     result = x/y
+#     print("x/y = ", result)
+# except ZeroDivisionError:
+#     print("Ділення на 0 забороненно!")
+# else:
+#     print("It is ok")
+# finally:
+#     print("Finally block")
+
+# for i in range(1, 100, 20):
+#     print(i)
+
+# squares = []
+# for value in range(1, 11):
+#     square = value**2
+#     squares.append(square)
+#     print("Squares", squares)
+
+
+# word = "Viktoria"
+# for w in word:
+#     # print(w)
+#     if "i" in w:
+#         print("Hello")
+
+# available_topping = ['mushrooms', 'olives', 'potato', 'pepers']
+# req_topping = []
+
+# favorite_languages = {
+#     "Dmitro": "python",
+#     "Nataly": "C",
+#     "Denys": "ruby",
+#     "Maxim": "PHP",
+#     "Nely": "acempler"
+# }
+# for name, lang in favorite_languages.items():
+#     print(name.title() + "'s favorit languages " + lang.title())
+
+users = {
+    "Boby": {
+        "first_name": "Bob",
+        "last_name": "File",
+        "location": "Las Vegas"
+    },
+    "Alise": {
+        "first_name": "Alise",
+        "last_name": "Erica",
+        "location": "New York"
+    }
+}
+
+for username, user_info in users.items():
+    print("Username" + username)
+    full_name = user_info["first_name"] + " " + user_info["last_name"]
