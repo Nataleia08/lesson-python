f9b264ccaf725816d78eb8cdd1bee7315ef962dd
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-10-31 21:38:13 +0200

    Com 2
    Mod 3 les 1 com 2

--- mod_3_lesson_1.py
+++ mod_3_lesson_1.py
@@ -8,4 +8,19 @@ from function import countdown, greeting, fibonachi, FizzBuzz, some_func
 # FizzBuzz()
 #some_func("Hello", 2, 4, "You", 3)
 
-countdown(10)
+# countdown(10)
+
+# Looking at the below code, write down the final values of A0, A1, ...An.
+
+A0 = dict(zip(('a', 'b', 'c', 'd', 'e'), (1, 2, 3, 4, 5)))
+A1 = range(10)
+A2 = sorted([i for i in A1 if i in A0])
+A3 = sorted([A0[s] for s in A0])
+A4 = [i for i in A1 if i in A3]
+A5 = {i: i*i for i in A1}
+A6 = [[i, i*i] for i in A1]
+A7 = [i if i % 2 else 0 for i in A1 if 2 < i < 8]
+
+print("A2:", A2)
+#print("A1:", A1)
+# print
